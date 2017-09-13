f45a7de990318f8b92d892de33bf22b3e0ca5a16
benhayehudi <bengreenberg@gmail.com>
2017-09-13 14:15:03 -0400

    added alt tags for contact images

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,9 +15,9 @@ class Contact extends React.Component {
                 <Col lg={12} md={12}>
                   <h2 id="title-linethree">I would love to hear from you.<br/> Please get in contact through any of the following:</h2>
                   <br /><br />
-                  <a href="https://www.facebook.com/RabbiBenGreenberg/"><img src="/facebook_icon.svg" width="20%"/></a>
-                  <a href="https://twitter.com/RabbiGreenberg"><img src="/twitter_icon.png" width="20%"/></a>
-                  <a href="https://www.linkedin.com/in/rabbigreenberg/"><img src="/linkedin_icon.png" width="22%"/></a>
+                  <a href="https://www.facebook.com/RabbiBenGreenberg/"><img src="/facebook_icon.svg" width="20%" alt="facebook icon"/></a>
+                  <a href="https://twitter.com/RabbiGreenberg"><img src="/twitter_icon.png" width="20%" alt="twitter icon"/></a>
+                  <a href="https://www.linkedin.com/in/rabbigreenberg/"><img src="/linkedin_icon.png" width="22%" alt="linkedin icon"/></a>
                   <br /><br /><br /><br /><br /><br /><br /><br />
                 </Col>
               </div>
