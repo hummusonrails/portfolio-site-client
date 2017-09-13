de1288af15dd298ffa91e80b8b755b84e31de9c9
benhayehudi <bengreenberg@gmail.com>
2017-09-13 19:18:36 -0400

    working on mobile responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,36 +3,27 @@ import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const NavbarInstance = (
-  <div>
-    <nav className="navbar navbar-default">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"    aria-expanded="false" aria-controls="navbar">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <a className="navbar-brand" href="/"></a>
-        </div>
-        <Navbar.Collapse>
-        <div id="navbar" className="navbar-collapse collapse">
-          <ul className="nav navbar-nav">
-            <li className="active"><LinkContainer to="/"><a href="/">Home</a></LinkContainer></li>
-            <li><LinkContainer to="/portfolio"><a href="/portfolio">Portfolio</a></LinkContainer></li>
-            <NavDropdown eventKey={3} title="Blog" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}><li><LinkContainer to="/blog/tech"><a href="/blog/tech">Tech Posts</a></LinkContainer></li></MenuItem>
-              <MenuItem eventKey={3.2}><li><LinkContainer to="/blog/jewish"><a href="/blog/jewish">Jewish Posts</a></LinkContainer></li></MenuItem>
-            </NavDropdown>
-          </ul>
-          <ul className="nav navbar-nav navbar-right">
-            <li className="active"><LinkContainer to="/contact"><a href="/contact">Contact <span className="sr-only">(current)</span></a></LinkContainer></li>
-          </ul>
-        </div>
-        </Navbar.Collapse>
-      </div>
-    </nav>
-  </div>
+  <Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">React-Bootstrap</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="/">Home</NavItem>
+        <NavItem eventKey={2} href="/portfolio">Portfolio</NavItem>
+        <NavDropdown eventKey={3} title="Blog" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1} href="/blog/tech">Tech Posts</MenuItem>
+          <MenuItem eventKey={3.2} href="/blog/jewish">Jewish Posts</MenuItem>
+        </NavDropdown>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="/contact">Contact</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
 
 export default NavbarInstance;
