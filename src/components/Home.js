17bbe5f068916f195fbd2fcf769fd3de89159cbe
benhayehudi <bengreenberg@gmail.com>
2017-09-13 20:34:42 -0400

    updated home component language

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,7 +22,7 @@ class Home extends React.Component {
                 <div id="index-content">
                   <p>I have worked for nearly 10 years as a communal rabbi, non-profit professional and community organizer. During the past decade I've served communities and organizations in Boston, Denver, Chicago and New York. Throughout my non-profit career I've been fortunate to work closely with college students on campus, organize for a better society, teach adult education and create and lead new departments and teams.</p>
                   <br />
-                  <p>After a decade of communal service, I've decided to shift my focus back to the tech sector. During the 1990s I co-founded and co-directed a San Diego, CA based computer security firm and co-founded Southern California's largest computer security conference. I've returned to my tech roots as a current student in the online immersive full stack web development program at the Flatiron School.</p>
+                  <p>After a decade of communal service, I've decided to shift my focus back to the tech sector. During the 1990s I co-founded and co-directed a San Diego, CA based computer security firm and co-founded Southern California's largest computer security conference. I've returned to my tech roots as a recent graduate of the online immersive full stack web development program at the Flatiron School.</p>
                   <br />
                   <p>Please browse my coding portfolio and take a look at my blog posts, both my tech articles and my posts about society, non-profit leadership and Jewish communal life written over the years.</p>
                   <br />
