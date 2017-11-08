96daae9a9c3c8ebf95c3b9963fef9a978954df6c
benhayehudi <bengreenberg@gmail.com>
2017-11-08 13:00:09 -0500

    setup basic structure of SPA

--- src/components/containers/SPA.js
+++ src/components/containers/SPA.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import fullpage from 'fullpage.js';
 import $ from 'jquery';
 import { loadTechBlog } from '../../actions/BlogActions';
 import { loadPortfolio } from '../../actions/PortfolioActions';
@@ -19,8 +18,79 @@ class SPA extends React.Component {
   render() {
 
     $(document).ready(function() {
-      $('#fullpage').fullpage();
-    });
+	   $('#fullpage').fullpage({
+  		//Navigation
+  		menu: '.navbar',
+  		lockAnchors: false,
+  		anchors:['firstPage', 'secondPage'],
+  		navigation: true,
+  		navigationPosition: 'right',
+  		navigationTooltips: ['', 'welcome', 'portfolio', 'blog', 'contact'],
+  		showActiveTooltip: true,
+  		slidesNavigation: false,
+  		slidesNavPosition: 'bottom',
+
+  		//Scrolling
+  		css3: true,
+  		scrollingSpeed: 700,
+  		autoScrolling: true,
+  		fitToSection: true,
+  		fitToSectionDelay: 1000,
+  		scrollBar: false,
+  		easing: 'easeInOutCubic',
+  		easingcss3: 'ease',
+  		loopBottom: false,
+  		loopTop: false,
+  		loopHorizontal: true,
+  		continuousVertical: false,
+  		continuousHorizontal: false,
+  		scrollHorizontally: false,
+  		interlockedSlides: false,
+  		dragAndMove: false,
+  		offsetSections: false,
+  		resetSliders: false,
+  		fadingEffect: false,
+  		normalScrollElements: '#element1, .element2',
+  		scrollOverflow: false,
+  		scrollOverflowReset: false,
+  		scrollOverflowOptions: null,
+  		touchSensitivity: 15,
+  		normalScrollElementTouchThreshold: 5,
+  		bigSectionsDestination: null,
+
+  		//Accessibility
+  		keyboardScrolling: true,
+  		animateAnchor: true,
+  		recordHistory: true,
+
+  		//Design
+  		controlArrows: true,
+  		verticalCentered: true,
+  		paddingTop: '3em',
+  		paddingBottom: '10px',
+  		fixedElements: '.navbar, .footer',
+  		responsiveWidth: 0,
+  		responsiveHeight: 0,
+  		responsiveSlides: false,
+  		parallax: false,
+  		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+
+  		//Custom selectors
+  		sectionSelector: '.section',
+  		slideSelector: '.slide',
+
+  		lazyLoading: true,
+
+  		//events
+  		onLeave: function(index, nextIndex, direction){},
+  		afterLoad: function(anchorLink, index){},
+  		afterRender: function(){},
+  		afterResize: function(){},
+  		afterResponsive: function(isResponsive){},
+  		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+  		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+	  });
+  });
 
     var sortedBlog =
       this.props.blogPosts.sort(function(posting1, posting2) {
