50bbb16aae37d68413528c225973a2f701187fe9
benhayehudi <bengreenberg@gmail.com>
2017-11-08 19:11:22 -0500

    new SPA page finished

--- src/actions/BlogActions.js
+++ src/actions/BlogActions.js
@@ -6,22 +6,22 @@ export function loadTechBlog() {
     data: JSON.stringify()
   };
 
-  fetch('https://galvanize-cors-proxy.herokuapp.com/https://bengreenberg.herokuapp.com/api/blog/tech.json', request)
+  fetch('https://bengreenberg.herokuapp.com/api/blog/tech.json', request)
     .then(data => data.json())
     .then(data => dispatch({ type: 'LOAD_BLOG', data: data }))
   }
 )}
 
-export function loadJewishBlog() {
-  return (dispatch => {
-
-  const request = {
-    method: 'get',
-    data: JSON.stringify()
-  };
-
-  fetch('https://bengreenberg.herokuapp.com/api/blog/jewish.json', request)
-    .then(data => data.json())
-    .then(data => dispatch({ type: 'LOAD_BLOG', data: data }))
-  }
-)}
+// export function loadJewishBlog() {
+//   return (dispatch => {
+//
+//   const request = {
+//     method: 'get',
+//     data: JSON.stringify()
+//   };
+//
+//   fetch('https://bengreenberg.herokuapp.com/api/blog/jewish.json', request)
+//     .then(data => data.json())
+//     .then(data => dispatch({ type: 'LOAD_BLOG', data: data }))
+//   }
+// )}
