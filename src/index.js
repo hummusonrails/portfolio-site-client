94b053f2eb5d7fa3e3f3a662c2e51875231c8fdd
benhayehudi <bengreenberg@gmail.com>
2017-11-20 18:21:24 -0500

    removed mediun blog reducer and action

--- src/index.js
+++ src/index.js
@@ -10,12 +10,10 @@ import './css/fullpage.css';
 import registerServiceWorker from './registerServiceWorker';
 import PortfolioReducer from './reducers/PortfolioReducer';
 import BlogReducer from './reducers/BlogReducer';
-import MediumBlogReducer from './reducers/MediumBlogReducer';
 
 const reducers = combineReducers({
     PortfolioReducer,
-    BlogReducer,
-    MediumBlogReducer
+    BlogReducer
 });
 
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk));
