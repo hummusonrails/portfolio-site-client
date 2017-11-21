93727b9679db2190b381e7028472805b0c504d16
benhayehudi <bengreenberg@gmail.com>
2017-11-21 17:10:17 -0500

    updated emojis in blog buttons and updated js function to only show share links for the specific post

--- src/components/blog/BlogIndexCard.js
+++ src/components/blog/BlogIndexCard.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import {emojify} from 'react-emojione';
 
 class BlogIndexCard extends React.Component {
 
   render() {
-    console.log(this.props.post)
 
     let blogSnippet =
       this.props.post ?
         this.props.post.content.slice(0, 500) : "Content is loading..."
 
-    let showShare = event => {
+    let showShare = (buttonId, event) => {
       event.preventDefault();
       let shareElement = document.getElementsByClassName("blog-share-section");
       for (let i = 0; i < shareElement.length; i++) {
-        if (shareElement[i].style.display === "block") {
-          shareElement[i].style.display = "none";
-        } else {
-          shareElement[i].style.display = "block";
+        if (shareElement[i].id == buttonId) {
+          if (shareElement[i].style.display === "block") {
+            shareElement[i].style.display = "none";
+          } else {
+            shareElement[i].style.display = "block";
+            shareElement[i].style.paddingTop = "5px";
+          }
+        }
       }
     }
-  }
 
     return (
       <div className="blog-card-wrapper">
@@ -35,15 +38,15 @@ class BlogIndexCard extends React.Component {
           <div className="blog-card-snippet">
             <span id="blog-date"><p>Published:<br/>{this.props.post.created_on}</p></span>
             <p>{blogSnippet}...</p>
-            <button id="blog-readmore"><a href={this.props.post.original_link} target="_new">👉 Read More</a></button>
+            <button id="blog-readmore"><a href={this.props.post.original_link} target="_new">{emojify(':point_right:')} Read More</a></button>
             &nbsp;&nbsp;
-            <button id="blog-share" onClick={(event) => showShare(event)}>🗣 Share This</button>
+            <button id="blog-share" className={this.props.post.id} onClick={(event) => showShare(this.props.post.id, event)}>{emojify(':speaking_head_in_silhouette:')} Share This</button>
           </div>
         </div>
-        <div className="blog-share-section">
-        <a href={`https://www.facebook.com/sharer/sharer.php?u=${this.props.post.original_link}`}><img src="facebook_icon.png" width="120px" alt="facebook share"/></a>
-        <a href={`https://twitter.com/home?status=Check%20out%20this%20article%20from%20the%20Reactive%20Rabbi%20at%20${this.props.post.original_link}`}><img src="twitter_icon.png" width="120px" alt="twitter share"/></a>
-        <a href={`https://www.linkedin.com/shareArticle?mini=true&url=${this.props.post.original_link}&title=The%20Article&summary=I%20found%20this%20article%20on%20the%20Reactive%20Rabbi%20(reactiverabbi.io)&source=`}><img src="linkedin_icon.png" width="120px" alt="linkedin share"/></a>
+        <div className="blog-share-section" id={this.props.post.id}>
+        <a href={`https://www.facebook.com/sharer/sharer.php?u=${this.props.post.original_link}`}><img src="facebook_icon.png" width="80px" alt="facebook share"/></a>
+        <a href={`https://twitter.com/home?status=Check%20out%20this%20article%20from%20the%20Reactive%20Rabbi%20at%20${this.props.post.original_link}`}><img src="twitter_icon.png" width="80px" alt="twitter share"/></a>
+        <a href={`https://www.linkedin.com/shareArticle?mini=true&url=${this.props.post.original_link}&title=The%20Article&summary=I%20found%20this%20article%20on%20the%20Reactive%20Rabbi%20(reactiverabbi.io)&source=`}><img src="linkedin_icon.png" width="80px" alt="linkedin share"/></a>
         </div>
         <div className="post-divider">
           <h1>. . . . . </h1>
