9e974e508fb2a61306d24fa0266950fbc360d813
benhayehudi <bengreenberg@gmail.com>
2017-11-30 12:45:39 -0500

    responsive styling for video cards on mobile

--- src/actions/VideoActions.js
+++ src/actions/VideoActions.js
@@ -6,7 +6,7 @@ export function loadVideos() {
     data: JSON.stringify()
   };
 
-  fetch('https://galvanize-cors-proxy.herokuapp.com/https://bengreenberg.herokuapp.com/api/videos/index.json', request)
+  fetch('https://bengreenberg.herokuapp.com/api/videos/index.json', request)
     .then(data => data.json())
     .then(data => dispatch({ type: 'LOAD_VIDEOS', data: data }))
   }
